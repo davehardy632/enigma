0b0b28e8c3abbd968a64cd22145af8b2ad7bfe5e
David Hardy <davehardy632@gmail.com>
2019-02-23 11:51:27 -0700

    Completed setup for project

---
+++ test/enigma_test.rb
@@ -0,0 +1,16 @@
+require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'date'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
