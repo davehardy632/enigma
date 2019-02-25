b014aa42a55fde7764d5f3c8bc67128203c662be
David Hardy <davehardy632@gmail.com>
2019-02-24 19:19:04 -0700

    Starting to reorganize and refactor

--- lib/enigma_helper_methods.rb
+++ lib/enigma_helper_methods.rb
@@ -1,5 +1,99 @@
 require 'date'
 module EnigmaHelperMethods
+  
+  def manual_key_helper(string) #manual entry
+    letter_keys = Hash.new(0)
+    string
+    letter_keys["A"] = string[0..1]
+    letter_keys["B"] = string[1..2]
+    letter_keys["C"] = string[2..3]
+    letter_keys["D"] = string[3..4]
+    letter_keys
+  end
 
+  def manual_keys(string) #manual entry
+    keys = {}
+    key_strings = manual_key_helper(string)
+    key_strings.each do |key, value|
+      keys[key] = value.to_i
+    end
+    keys
+  end
+
+  def manual_offset_helper(date) #manual
+    organize = []
+    offset_key = date.to_i * date.to_i
+    organize << offset_key.to_s.split(//)
+    organize.flatten
+  end
+
+  def manual_offsets(date) # final offset values in a hash
+      values = manual_offset_helper(date)
+      offset_keys = {}
+      offset_to_int = {}
+      offset_keys["A"] = values[-4]
+      offset_keys["B"] = values[-3]
+      offset_keys["C"] = values[-2]
+      offset_keys["D"] = values[-1]
+      offset_keys.each do |key, value|
+        offset_to_int[key] = value.to_i
+    end
+      offset_to_int #potential naming issue
+  end
+
+  def manual_total_rotation(key, date) #default used in final encrypt method
+    final_rotation = {}
+    keys = manual_keys(key)
+    offsets = manual_offsets(date)
+    keys.merge(offsets) do |key, key_value, offset_value|
+      final_rotation[key] = key_value + offset_value
+    end
+    final_rotation
+  end
+
+  def encrypt_letter(letter, number) # not needed in final method #default
+    new_letter = letter.tr(@alphabet.join, @alphabet.rotate(number).join)
+    new_letter
+  end
+
+  def decrypt_letter(letter, number) # not needed in final method #default
+  letter = letter.tr(@alphabet.rotate(number).join ,@alphabet.join)
+  end
+
+  def manually_encrypt_message(message, key, date)
+    split_message = message.downcase.split(//)
+    new = []
+    total_rotation = manual_total_rotation(key, date)
+    split_message.each_with_index do |letter, index|
+        if index == 0 || index % 4 == 0
+      new << encrypt_letter(letter, total_rotation["A"])
+    elsif index == 1 || index % 4 == 1
+      new << encrypt_letter(letter, total_rotation["B"])
+    elsif index == 2 || index % 4 == 2
+      new << encrypt_letter(letter, total_rotation["C"])
+    elsif index == 3 || index % 4 == 3
+      new << encrypt_letter(letter, total_rotation["D"])
+      end
+    end
+    new.join
+  end
+
+  def manually_decrypt_message(message, key, date)
+    split_message = message.downcase.split(//)
+    new = []
+    total_rotation = manual_total_rotation(key, date)
+    split_message.each_with_index do |letter, index|
+        if index == 0 || index % 4 == 0
+      new << decrypt_letter(letter, total_rotation["A"])
+    elsif index == 1 || index % 4 == 1
+      new << decrypt_letter(letter, total_rotation["B"])
+    elsif index == 2 || index % 4 == 2
+      new << decrypt_letter(letter, total_rotation["C"])
+    elsif index == 3 || index % 4 == 3
+      new << decrypt_letter(letter, total_rotation["D"])
+      end
+    end
+    new.join
+  end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -20,44 +20,6 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.alphabet
   end
 
-  def test_manual_key_helper # manual entry
-    assert_equal Hash, @enigma.manual_key_helper("02715").class
-  end
-
-  def test_manual_keys #manual entry
-    assert_equal Hash, @enigma.manual_keys("02715").class
-  end
-
-  def test_manual_offset_helper #manual
-    expected = ["1", "6", "7", "2", "4", "0", "1", "0", "2", "5"]
-
-    assert_equal expected, @enigma.manual_offset_helper("040895")
-  end
-
-  def test_manual_offsets
-    expected = {"A"=>1, "B"=>0, "C"=>2, "D"=>5}
-
-    assert_equal expected, @enigma.manual_offsets("040895")
-  end
-
-  def test_manual_total_rotation
-    expected = {"A"=>3, "B"=>27, "C"=>73, "D"=>20}
-
-    assert_equal expected, @enigma.manual_total_rotation("02715","040895")
-  end
-
-  def test_manually_encrypt_message #manual
-    assert_equal "keder ohulw", @enigma.manually_encrypt_message("hello world", "02715", "040895")
-  end
-
-  def test_decrypt_letter
-    assert_equal "e", @enigma.decrypt_letter("h", 3)
-  end
-
-  def test_manally_decrypt_message
-    assert_equal "hello world", @enigma.manually_decrypt_message("keder ohulw", "02715", "040895")
-  end
-
   def test_encrypt # good
     expected = {
          encryption: "keder ohulw",
